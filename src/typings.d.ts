57b6e85f3fc17c4c4ab6ca6049b25dc4dd9a5ada
Leonid Agranovskiy <leonid.agranovskiy@prodyna.com>
2016-12-01 09:59:17 +0100

    declare var require: any;

--- src/typings.d.ts
+++ src/typings.d.ts
@@ -2,6 +2,7 @@
 // https://github.com/typings/typings
 // https://www.typescriptlang.org/docs/handbook/writing-declaration-files.html
 
+declare var require: any;
 declare var System: any;
 declare var moment: any;
 declare var Auth0Lock: any;

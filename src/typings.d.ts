eaf2d92d506dc2f990adbcf2a33301d861f03038
Leonid Agranovskiy <leonid.agranovskiy@prodyna.com>
2016-12-01 10:29:48 +0100

    trying to fix build problems on heroku

--- src/typings.d.ts
+++ src/typings.d.ts
@@ -2,9 +2,13 @@
 // https://github.com/typings/typings
 // https://www.typescriptlang.org/docs/handbook/writing-declaration-files.html
 
+/// <reference path="../node_modules/@types/googlemaps/index.d.ts" />
+
+'use strict';
+
 declare var require: any;
 declare var System: any;
 declare var moment: any;
 declare var Auth0Lock: any;
 
-/// <reference path="../node_modules/@types/googlemaps/index.d.ts" />
+
